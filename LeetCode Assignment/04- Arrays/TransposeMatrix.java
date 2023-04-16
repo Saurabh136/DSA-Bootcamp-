420f1c92701748fb0490ea68cfe59484338796de
Saurabh Mhamunkar <smhamunkar2610@hotmail.com>
2023-04-16 16:49:09 +0530

    Update TransposeMatrix.java

--- LeetCode Assignment/04- Arrays/TransposeMatrix.java
+++ LeetCode Assignment/04- Arrays/TransposeMatrix.java
@@ -12,12 +12,16 @@ public class TransposeMatrix {
         
     }
     public static int[][] transpose(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] ans = new int[n][m];
-        for(int i = 0; i < m; i++) {    // for rows
-            for(int j = 0; j < n; j++) {  // for columns
-                ans[j][i] = matrix[i][j];
+        int m = matrix.length;  // no. of rows
+        int n = matrix[0].length; // no. of columns
+        int[][] ans = new int[n][m]; // 2D integer array with m and n dimensions
+        for(int i = 0; i < m; i++) {    // Iterates rows
+            for(int j = 0; j < n; j++) {  // Iterates columns
+                ans[j][i] = matrix[i][j];  
+                // Since the rows and columns are swapped in the transposed matrix, 
+                //the row index i in the input matrix corresponds to the column index in the transposed matrix, 
+                //while the column index j in the input matrix corresponds to the row index in the transposed matrix. 
+                //Thus, the function assigns matrix[i][j] to ans[j][i].
             }
         }
         return ans;
